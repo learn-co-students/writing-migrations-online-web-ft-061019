6b48cba89fd6db29ec80247e02629ce06eeb22be
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-26 03:31:37 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,1 +1,1 @@
-class CreateStudents < ActiveRecord::Migration[4.2]  def change    create_table :students do |t|      t.string :name      end	 endend
+class CreateStudents < ActiveRecord::Migration[5.1]    def change    create_table :students do |t|      t.string :name    end	end	end
